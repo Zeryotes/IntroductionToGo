e60b3f814b3fcd4bea50c311ec7a573eb93afee7
Jordão de Souza Lopes <jordaodesouzalopes@gmail.com>
2022-02-05 10:11:57 -0300

    Correções na apresentação de mensagens e scoreboarding

--- main.go
+++ main.go
@@ -15,39 +15,45 @@ func initializer(state chan int) {
 
 // Goroutine do jogador1
 func player1(state chan int) {
-	fmt.Println("----------- PLAYER 1 -----------")
+	// fmt.Println("----------- PLAYER 1 -----------")
+
+	// Seed para garantir aleatoriedade entre as jogadas
 	rand.Seed(time.Now().UnixNano() + 5)
+
 	stateNow := <-state
 	chanceOnPlay := rand.Intn(1000) + 1
 	if stateNow == 0 {
-		fmt.Println("PLAYER 1 SACA A BOLA")
+		// fmt.Println("PLAYER 1 SACA A BOLA")
 	} else {
 		if isScore(stateNow) {
 			SCOREPLAYER1++
-			fmt.Printf("PLAYER 1 SCORED (%d)\n", SCOREPLAYER1)
+			// fmt.Printf("PLAYER 1 SCORED (%d)\n", SCOREPLAYER1)
 			chanceOnPlay = 0
 		}
 	}
-	fmt.Println("Chance da jogada:", chanceOnPlay)
+	// fmt.Println("Chance da jogada:", chanceOnPlay)
 	state <- chanceOnPlay
 }
 
 // Goroutine do jogador2
 func player2(state chan int) {
-	fmt.Println("----------- PLAYER 2 -----------")
+	// fmt.Println("----------- PLAYER 2 -----------")
+
+	// Seed para garantir aleatoriedade entre as jogadas
 	rand.Seed(time.Now().UnixNano() + 10)
+
 	stateNow := <-state
 	chanceOnPlay := rand.Intn(1000) + 1
 	if stateNow == 0 {
-		fmt.Println("PLAYER 2 SACA A BOLA")
+		// fmt.Println("PLAYER 2 SACA A BOLA")
 	} else {
 		if isScore(stateNow) {
 			SCOREPLAYER2++
-			fmt.Printf("PLAYER 2 SCORED (%d)\n", SCOREPLAYER2)
+			// fmt.Printf("PLAYER 2 SCORED (%d)\n", SCOREPLAYER2)
 			chanceOnPlay = 0
 		}
 	}
-	fmt.Println("Chance da jogada:", chanceOnPlay)
+	// fmt.Println("Chance da jogada:", chanceOnPlay)
 	state <- chanceOnPlay
 }
 
@@ -77,35 +83,21 @@ func startGame(channel chan int) {
 	go initializer(channel)
 	for i := 1; i <= MATCH; i++ {
 		fmt.Printf("MATCH %d STARTED!!!\n", i)
+		// Cada novo MATCH reinicia os valores de SET de cada PLAYER
 		SETPLAYER1 = 0
 		SETPLAYER2 = 0
 		for j := 1; j <= SET; j++ {
 			fmt.Printf("SET %d STARTED!!!\n", j)
-			if SETPLAYER1 >= (SET/2)+1 {
-				fmt.Printf("PLAYER1 WINS MATCH %d!!! P1 (%d) Vs (%d) P2\n", i, SETPLAYER1, SETPLAYER2)
-				break
-			}
-			if SETPLAYER2 >= (SET/2)+1 {
-				fmt.Printf("PLAYER2 WINS MATCH %d!!! P1 (%d) Vs (%d) P2\n", i, SETPLAYER1, SETPLAYER2)
-				break
-			}
+			// Cada novo SET reinicia os valores de GAME de cada PLAYER
 			GAMEPLAYER1 = 0
 			GAMEPLAYER2 = 0
 			for k := 1; k <= GAME; k++ {
 				fmt.Printf("GAME %d STARTED!!!\n", k)
-				if GAMEPLAYER1 >= (GAME/2)+1 {
-					fmt.Printf("PLAYER1 WINS SET %d!!! P1 (%d) Vs (%d) P2\n", j, GAMEPLAYER1, GAMEPLAYER2)
-					SETPLAYER1++
-					break
-				}
-				if GAMEPLAYER2 >= (GAME/2)+1 {
-					fmt.Printf("PLAYER2 WINS SET %d!!! P1 (%d) Vs (%d) P2\n", j, GAMEPLAYER1, GAMEPLAYER2)
-					SETPLAYER2++
-					break
-				}
+				// Cada novo GAME reinicia os SCORES de cada PLAYER
 				SCOREPLAYER1 = 0
 				SCOREPLAYER2 = 0
 				for {
+					// Continua chamando goroutinas até um dos PLAYERS ganharem o GAME
 					if SCOREPLAYER1 >= SCORE && SCOREPLAYER1 > SCOREPLAYER2+2 {
 						fmt.Printf("PLAYER1 WINS GAME %d!!! P1 (%d) Vs (%d) P2\n", k, SCOREPLAYER1, SCOREPLAYER2)
 						scores[i-1][j-1][k-1][0] = SCOREPLAYER1
@@ -124,9 +116,37 @@ func startGame(channel chan int) {
 					go player2(channel)
 					time.Sleep(time.Duration(math.Pow10(SLEEPINGTIME)))
 				}
+				// Verifica quem ganhou o SET após uma sequencia de GAMES
+				if GAMEPLAYER1 >= (GAME/2)+1 {
+					fmt.Printf("PLAYER1 WINS SET %d!!! P1 (%d) Vs (%d) P2\n", j, GAMEPLAYER1, GAMEPLAYER2)
+					SETPLAYER1++
+					break
+				}
+				if GAMEPLAYER2 >= (GAME/2)+1 {
+					fmt.Printf("PLAYER2 WINS SET %d!!! P1 (%d) Vs (%d) P2\n", j, GAMEPLAYER1, GAMEPLAYER2)
+					SETPLAYER2++
+					break
+				}
 				fmt.Printf("GAME %d ENDED!!!\n", k)
 			}
+			// Verifica quem ganhou a MATCH após uma sequencia de SET's
+			if SETPLAYER1 >= (SET/2)+1 {
+				fmt.Printf("PLAYER1 WINS MATCH %d!!! P1 (%d) Vs (%d) P2\n", i, SETPLAYER1, SETPLAYER2)
+				MATCHPLAYER1++
+				break
+			}
+			if SETPLAYER2 >= (SET/2)+1 {
+				fmt.Printf("PLAYER2 WINS MATCH %d!!! P1 (%d) Vs (%d) P2\n", i, SETPLAYER1, SETPLAYER2)
+				MATCHPLAYER2++
+				break
+			}
 			fmt.Printf("SET %d ENDED!!!\n", j)
+		}
+		if MATCHPLAYER1 >= (MATCH/2)+1 {
+			break
+		}
+		if MATCHPLAYER2 >= (MATCH/2)+1 {
+			break
 		}
 		fmt.Printf("MATCH %d ENDED!!!\n", i)
 	}
@@ -135,11 +155,11 @@ func startGame(channel chan int) {
 
 // Imprime o scoreboard final da partida
 func printScores(scores [][][][]int) {
-	fmt.Println("-----[SCORE BOARDING]-----")
+	fmt.Println("-----[SCORE BOARDING]---------------")
 	for i := range scores {
-		fmt.Printf("----- MATCH %d -----\n", i+1)
+		fmt.Printf("----- MATCH %d ----------------------\n", i+1)
 		for j := range scores[i] {
-			fmt.Printf("----- SET %d -----\n", j+1)
+			fmt.Printf("----- SET %d ------------------------\n", j+1)
 			for k := 1; k <= GAME; k++ {
 				fmt.Printf("[%d] ", k)
 			}
@@ -163,6 +183,15 @@ func printScores(scores [][][][]int) {
 			fmt.Println("PLAYER 2")
 		}
 	}
+	fmt.Println("------------------------------------")
+	if MATCHPLAYER1 > MATCHPLAYER2 {
+		fmt.Printf("PLAYER 1 WIN!!! %d MATCH(S) a %d\n", MATCHPLAYER1, MATCHPLAYER2)
+	} else if MATCHPLAYER2 > MATCHPLAYER1 {
+		fmt.Printf("PLAYER 2 WIN!!! %d MATCH(S) a %d\n", MATCHPLAYER2, MATCHPLAYER1)
+	} else {
+		fmt.Printf("EMPATE!!! P1 %d MATCH(S) a %d P2\n", MATCHPLAYER1, MATCHPLAYER2)
+	}
+
 }
 
 // Apresenta a tela inicial para o usuário
@@ -179,10 +208,10 @@ func systemPresentation() {
 	fmt.Println(line)
 }
 
-// Define as configurações da partida. (PARTE EXTRA)
+// Define as configurações da partida.
 func setConfiguration() {
 	fmt.Println("Configurando a partida!")
-	fmt.Println("Digite a quantidade de pontos para definir um GAME: ")
+	fmt.Println("Digite a quantidade de SCORES para definir um GAME: ")
 	fmt.Scanln(&SCORE)
 	fmt.Println("Digite a quantidade de GAMES para definir um SET: ")
 	fmt.Scanln(&GAME)
@@ -200,15 +229,17 @@ func setConfiguration() {
 var SCORE = 4
 var SCOREPLAYER1 = 0
 var SCOREPLAYER2 = 0
-var GAME = 5
+var GAME = 1
 var GAMEPLAYER1 = 0
 var GAMEPLAYER2 = 0
-var SET = 3
+var SET = 1
 var SETPLAYER1 = 0
 var SETPLAYER2 = 0
 var MATCH = 1
+var MATCHPLAYER1 = 0
+var MATCHPLAYER2 = 0
 var SUCESSCHANCE = 200 // Valor entre 2 a 999 (define a chance de marcar ponto)
-var SLEEPINGTIME = 8   // Valor entre 0 a 10 (define o timer do log no terminar)
+var SLEEPINGTIME = 8   // Valor entre 0 a 10 (define a espera entre cada goroutina)
 
 func main() {
 	channel := make(chan int)
